a44ff2e0a3e050864a2ba909a8039dd1c6a1bfe6
jim <jim.zhu@ubtrobot.com>
2022-05-13 17:33:11 +0800

    * clang-format 整理了下代码格式

--- muduo/base/BoundedBlockingQueue.h
+++ muduo/base/BoundedBlockingQueue.h
@@ -6,96 +6,87 @@
 #ifndef MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
 #define MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
 
-#include "muduo/base/Condition.h"
-#include "muduo/base/Mutex.h"
+#include <assert.h>
 
 #include <boost/circular_buffer.hpp>
-#include <assert.h>
+
+#include "muduo/base/Condition.h"
+#include "muduo/base/Mutex.h"
 
 namespace muduo
 {
 
-template<typename T>
-class BoundedBlockingQueue : noncopyable
+template <typename T> class BoundedBlockingQueue : noncopyable
 {
- public:
-  explicit BoundedBlockingQueue(int maxSize)
-    : mutex_(),
-      notEmpty_(mutex_),
-      notFull_(mutex_),
-      queue_(maxSize)
-  {
-  }
-
-  void put(const T& x)
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.full())
+   public:
+    explicit BoundedBlockingQueue(int maxSize)
+        : mutex_()
+        , notEmpty_(mutex_)
+        , notFull_(mutex_)
+        , queue_(maxSize)
     {
-      notFull_.wait();
     }
-    assert(!queue_.full());
-    queue_.push_back(x);
-    notEmpty_.notify();
-  }
 
-  void put(T&& x)
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.full())
+    void put(const T& x)
     {
-      notFull_.wait();
+        MutexLockGuard lock(mutex_);
+        while (queue_.full()) { notFull_.wait(); }
+        assert(!queue_.full());
+        queue_.push_back(x);
+        notEmpty_.notify();
     }
-    assert(!queue_.full());
-    queue_.push_back(std::move(x));
-    notEmpty_.notify();
-  }
 
-  T take()
-  {
-    MutexLockGuard lock(mutex_);
-    while (queue_.empty())
+    void put(T&& x)
     {
-      notEmpty_.wait();
+        MutexLockGuard lock(mutex_);
+        while (queue_.full()) { notFull_.wait(); }
+        assert(!queue_.full());
+        queue_.push_back(std::move(x));
+        notEmpty_.notify();
     }
-    assert(!queue_.empty());
-    T front(std::move(queue_.front()));
-    queue_.pop_front();
-    notFull_.notify();
-    return front;
-  }
 
-  bool empty() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.empty();
-  }
+    T take()
+    {
+        MutexLockGuard lock(mutex_);
+        while (queue_.empty()) { notEmpty_.wait(); }
+        assert(!queue_.empty());
+        T front(std::move(queue_.front()));
+        queue_.pop_front();
+        notFull_.notify();
+        return front;
+    }
 
-  bool full() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.full();
-  }
+    bool empty() const
+    {
+        MutexLockGuard lock(mutex_);
+        return queue_.empty();
+    }
 
-  size_t size() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.size();
-  }
+    bool full() const
+    {
+        MutexLockGuard lock(mutex_);
+        return queue_.full();
+    }
 
-  size_t capacity() const
-  {
-    MutexLockGuard lock(mutex_);
-    return queue_.capacity();
-  }
+    size_t size() const
+    {
+        MutexLockGuard lock(mutex_);
+        return queue_.size();
+    }
 
- private:
-  mutable MutexLock          mutex_;
-  Condition                  notEmpty_ GUARDED_BY(mutex_);
-  Condition                  notFull_ GUARDED_BY(mutex_);
-  boost::circular_buffer<T>  queue_ GUARDED_BY(mutex_);
+    size_t capacity() const
+    {
+        MutexLockGuard lock(mutex_);
+        return queue_.capacity();
+    }
+
+   private:
+    mutable MutexLock mutex_;
+    Condition notEmpty_ GUARDED_BY(mutex_);
+    Condition notFull_ GUARDED_BY(mutex_);
+    boost::circular_buffer<T> queue_ GUARDED_BY(mutex_);
 };
 
-}  // namespace muduo
+} // namespace muduo
 
-#endif  // MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
+#endif // MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
